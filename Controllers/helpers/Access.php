7b61a95db31f8314513b0d0ff68dc3a7274907aa
Thomas Lauria <thomas@marcmittag.de>
2014-04-30 15:49:24 +0200

    created a interface for Access Plugins

--- Controllers/helpers/Access.php
+++ Controllers/helpers/Access.php
@@ -36,11 +36,9 @@
  * @package translate5
  * @version 1.0
  *
- */
-/* 
  * Helper, der bei jedem http-request prüft, ob der Benutzer noch authentifiziert ist
  */
-class ZfExtended_Controller_Helper_Access extends  Zend_Controller_Action_Helper_Abstract {
+class ZfExtended_Controller_Helper_Access extends Zend_Controller_Action_Helper_Abstract implements ZfExtended_Controller_Helper_IAccess  {
     /**
      * Zend_Controller_Front
      */
@@ -94,8 +92,6 @@ class ZfExtended_Controller_Helper_Access extends  Zend_Controller_Action_Helper
         }
         
         try {
-                        
-                        
             if(!$this->_acl->isInAllowedRoles(
                     $this->_roles, 
                     $module.$this->_request->getControllerName(), 
