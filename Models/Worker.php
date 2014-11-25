f03608c8902f79601693367d8e4307c6e45252ed
Thomas Lauria <thomas@marcmittag.de>
2014-11-25 16:58:47 +0100

    TRANSLATE-22: fixes for TRANSLATE-303, trigger initial run after queuing

--- Models/Worker.php
+++ Models/Worker.php
@@ -140,6 +140,10 @@ class ZfExtended_Models_Worker extends ZfExtended_Models_Entity_Abstract {
             error_log(__CLASS__.' -> '.__FUNCTION__.' workerModel can not wake up next scheduled worker with $id='.$id);
             return false;
         }
+        
+        $async = ZfExtended_Factory::get('ZfExtended_Worker_TriggerByHttp');
+        /* @var $async ZfExtended_Worker_TriggerByHttp */
+        $async->triggerQueue();
     }
     
     
