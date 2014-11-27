93e229664b7b0d3e256f22a36c46d6bb51c31138
Thomas Lauria <thomas@marcmittag.de>
2014-11-27 16:25:37 +0100

    TRANSLATE-301: unified meta attributes for task and segments

---
+++ Models/Entity/MetaAbstract.php
@@ -0,0 +1,45 @@
+<?php
+ /*
+   START LICENSE AND COPYRIGHT
+
+  This file is part of the ZfExtended library and build on Zend Framework
+
+  Copyright (c) 2013 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
+
+  Contact:  http://www.MittagQI.com/  /  service (ÄTT) MittagQI.com
+
+  This file is multi-licensed under the EPL, LGPL and GPL licenses. 
+
+  It may be used under the terms of the Eclipse Public License - v 1.0
+  as published by the Eclipse Foundation and appearing in the file eclipse-license.txt 
+  included in the packaging of this file.  Please review the following information 
+  to ensure the Eclipse Public License - v 1.0 requirements will be met:
+  http://www.eclipse.org/legal/epl-v10.html.
+
+  Also it may be used under the terms of the GNU LESSER GENERAL PUBLIC LICENSE version 3
+  or the GNU GENERAL PUBLIC LICENSE version 3 as published by the 
+  Free Software Foundation, Inc. and appearing in the files lgpl-license.txt and gpl3-license.txt
+  included in the packaging of this file.  Please review the following information 
+  to ensure the GNU LESSER GENERAL PUBLIC LICENSE version 3 requirements or the
+  GNU GENERAL PUBLIC LICENSE version 3 requirements will be met:
+  http://www.gnu.org/licenses/lgpl.html   /  http://www.gnu.org/licenses/gpl.html
+  
+  @copyright  Marc Mittag, MittagQI - Quality Informatics
+  @author     MittagQI - Quality Informatics
+  @license    Multi-licensed under Eclipse Public License - v 1.0 http://www.eclipse.org/legal/epl-v10.html, GNU LESSER GENERAL PUBLIC LICENSE version 3 http://www.gnu.org/licenses/lgpl.html and GNU GENERAL PUBLIC LICENSE version 3 http://www.gnu.org/licenses/gpl.html
+
+  END LICENSE AND COPYRIGHT 
+ */
+
+abstract class ZfExtended_Models_Entity_MetaAbstract extends ZfExtended_Models_Entity_Abstract {
+    const META_TYPE_STRING = 'string';
+    const META_TYPE_BOOLEAN = 'boolean';
+    const META_TYPE_INTEGER = 'integer';
+    const META_TYPE_FLOAT = 'float';
+    
+    public function addMeta($name, $type, $default, $comment, $length = 0) {
+        if(! $this->hasField($name)) {
+            call_user_func_array(array($this->db, 'addColumn'), func_get_args());
+        }
+    }
+}
