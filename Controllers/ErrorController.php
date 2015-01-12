e6b35e2b380e849fb65e7499f00f554b3562b63b
Marc Mittag <marc@mittagqi.com>
2015-01-12 17:43:26 +0100

    add Wrapper Class for using additional, normal routes, which fake REST-Routes  * - these Routes should be treated as REST-Routes  * - but take care for there REST-full behaviour by themselves and not through REST_Controller_Plugin_RestHandler as ZfExtended_Controller_RestLikeRoute do)

--- Controllers/ErrorController.php
+++ Controllers/ErrorController.php
@@ -302,7 +302,7 @@ class ErrorController extends ZfExtended_Controllers_Action
      */
     protected function isRestRoute(){
         $restRoute = 'Zend_Rest_Route';
-        if($this->route === $restRoute || $this->route === 'ZfExtended_Controller_RestLikeRoute'){
+        if($this->route === $restRoute || $this->route === 'ZfExtended_Controller_RestLikeRoute' || $this->route === 'ZfExtended_Controller_RestFakeRoute'){
             return true;
         }
         return is_subclass_of($this->route, $restRoute);
