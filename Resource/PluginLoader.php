c5a5471932aed8d36d23d3eab0b944e43af90265
Stephan Bergmann <Stephan@MittagQI.com>
2015-01-22 10:20:23 +0100

    TRANSLATE-330: registered plugin which does log a message to error-log NOT trow an exception

--- Resource/PluginLoader.php
+++ Resource/PluginLoader.php
@@ -1,63 +1,42 @@
 <?php
- /*
-   START LICENSE AND COPYRIGHT
-
-  This file is part of the ZfExtended library and build on Zend Framework
-
-  Copyright (c) 2013 Marc Mittag; MittagQI - Quality Informatics;  All rights reserved.
-
-  Contact:  http://www.MittagQI.com/  /  service (ÄTT) MittagQI.com
-
-  This file is multi-licensed under the EPL, LGPL and GPL licenses. 
-
-  It may be used under the terms of the Eclipse Public License - v 1.0
-  as published by the Eclipse Foundation and appearing in the file eclipse-license.txt 
-  included in the packaging of this file.  Please review the following information 
-  to ensure the Eclipse Public License - v 1.0 requirements will be met:
-  http://www.eclipse.org/legal/epl-v10.html.
-
-  Also it may be used under the terms of the GNU LESSER GENERAL PUBLIC LICENSE version 3
-  or the GNU GENERAL PUBLIC LICENSE version 3 as published by the 
-  Free Software Foundation, Inc. and appearing in the files lgpl-license.txt and gpl3-license.txt
-  included in the packaging of this file.  Please review the following information 
-  to ensure the GNU LESSER GENERAL PUBLIC LICENSE version 3 requirements or the
-  GNU GENERAL PUBLIC LICENSE version 3 requirements will be met:
-  http://www.gnu.org/licenses/lgpl.html   /  http://www.gnu.org/licenses/gpl.html
-  
-  @copyright  Marc Mittag, MittagQI - Quality Informatics
-  @author     MittagQI - Quality Informatics
-  @license    Multi-licensed under Eclipse Public License - v 1.0 http://www.eclipse.org/legal/epl-v10.html, GNU LESSER GENERAL PUBLIC LICENSE version 3 http://www.gnu.org/licenses/lgpl.html and GNU GENERAL PUBLIC LICENSE version 3 http://www.gnu.org/licenses/gpl.html
-
-  END LICENSE AND COPYRIGHT 
+/*
+ * START LICENSE AND COPYRIGHT This file is part of the ZfExtended library and build on Zend Framework Copyright (c) 2013 Marc Mittag; MittagQI - Quality Informatics; All rights reserved. Contact: http://www.MittagQI.com/ / service (ÄTT) MittagQI.com This file is multi-licensed under the EPL, LGPL and GPL licenses. It may be used under the terms of the Eclipse Public License - v 1.0 as published by the Eclipse Foundation and appearing in the file eclipse-license.txt included in the packaging of this file. Please review the following information to ensure the Eclipse Public License - v 1.0 requirements will be met: http://www.eclipse.org/legal/epl-v10.html. Also it may be used under the terms of the GNU LESSER GENERAL PUBLIC LICENSE version 3 or the GNU GENERAL PUBLIC LICENSE version 3 as published by the Free Software Foundation, Inc. and appearing in the files lgpl-license.txt and gpl3-license.txt included in the packaging of this file. Please review the following information to ensure the GNU LESSER GENERAL PUBLIC LICENSE version 3 requirements or the GNU GENERAL PUBLIC LICENSE version 3 requirements will be met: http://www.gnu.org/licenses/lgpl.html / http://www.gnu.org/licenses/gpl.html @copyright Marc Mittag, MittagQI - Quality Informatics @author MittagQI - Quality Informatics @license Multi-licensed under Eclipse Public License - v 1.0 http://www.eclipse.org/legal/epl-v10.html, GNU LESSER GENERAL PUBLIC LICENSE version 3 http://www.gnu.org/licenses/lgpl.html and GNU GENERAL PUBLIC LICENSE version 3 http://www.gnu.org/licenses/gpl.html END LICENSE AND COPYRIGHT
  */
 
-/**#@+
+/**
+ * #@+
+ * 
  * @author Marc Mittag
  * @package ZfExtended
  * @version 2.0
- *
+ *         
  */
 /**
  */
 /**
  * Initialize all plugins wich should be loaded.
  * They are defined in Zf_configuration-list runtimeOptions.plugins.active
- *
  */
-class ZfExtended_Resource_PluginLoader extends Zend_Application_Resource_ResourceAbstract{
-    
-    public function init(){
+class ZfExtended_Resource_PluginLoader extends Zend_Application_Resource_ResourceAbstract {
+    public function init() {
         $config = Zend_Registry::get('config');
-        if(!isset($config->runtimeOptions->plugins)){
+        if (! isset($config->runtimeOptions->plugins)) {
             return;
         }
-        $pluginClasses = $config->runtimeOptions->plugins->active->toArray();
-
-        //FIXME: ensure that a plugin is only loaded once!!!
-
-        foreach ($pluginClasses as $pluginClass){
-            //error_log("Plugin-Class ".$pluginClass." initialized.");
-            ZfExtended_Factory::get($pluginClass);
+        $pluginClasses = array_unique($config->runtimeOptions->plugins->active->toArray());
+        
+        foreach ($pluginClasses as $pluginClass) {
+            // error_log("Plugin-Class ".$pluginClass." initialized.");
+            try {
+                ZfExtended_Factory::get($pluginClass);
+            }
+            catch (ReflectionException $exception) {
+                // FIXME TRANSLATE-330
+                //$log = ZfExtended_Factory::get('ZfExtended_Log');
+                /* @var $log ZfExtended_Log */
+                //$log->logError('Can not load plugin '.$pluginClass, __CLASS__.' -> '.__FUNCTION__.'; $exception: '. print_r($exception->getMessage(), true));
+                error_log(__CLASS__.' -> '.__FUNCTION__.'; $exception: '. print_r($exception->getMessage(), true));
+            }
         }
     }
 }
