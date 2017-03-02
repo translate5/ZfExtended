2cd7d336e29d51aae70d555c728d857897749ed6
Thomas Lauria <thomas@mittagqi.com>
2017-03-02 10:03:48 +0100

    TRANSLATE-820 Generalization of Languages model - refactoring

--- Languages.php
+++ Languages.php
@@ -200,25 +200,21 @@ class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract {
     }
 
     /***
-     * Order languages based on the zf config languages order (ex. DE,FR,IT,MK ...)
+     * Reorders a RFC5646 language list based to the order given in $preordered (ex. DE,FR,IT,MK ...)
      * @param array $languages
-     * @param string $zfconfiglangs
+     * @param array $preorderd
      * @return array
      */
-    public function orderLanguages($languages,$zfconfiglangs){
-        if(!isset($zfconfiglangs)){
+    public function orderLanguages($languages, array $preorderd){
+        if(empty($preorderd)){
             return $languages;
         }
-        $zfconfiglangs=$zfconfiglangs->toArray();
-        if(empty($zfconfiglangs)){
-            return $languages;
-        }
-        foreach ($zfconfiglangs as $lng){
+        foreach ($preorderd as $lng){
             $oldIndex = array_search($lng, array_column($languages, 1));
             if(!$oldIndex){
                 continue;
             }
-            $newIndex =array_search($lng, $zfconfiglangs);
+            $newIndex =array_search($lng, $preorderd);
             $tmp = $languages[$newIndex];
             $languages[$newIndex] =$languages[$oldIndex];
             $languages[$oldIndex] = $tmp;
