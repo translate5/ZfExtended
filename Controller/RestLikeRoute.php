f707a261f8da7e6185de521689f7f34da584edc3
Thomas Lauria <thomas@marcmittag.de>
2018-03-05 14:17:49 +0100

    improve comments about difference of restfake and restlike routes

--- Controller/RestLikeRoute.php
+++ Controller/RestLikeRoute.php
@@ -29,7 +29,15 @@ END LICENSE AND COPYRIGHT
  * 
  */
 /**
- * Wrapper Class for using additional, normal routes in a RESTful Context
+ * Wrapper Class for using additional, normal routes, which fake REST-Routes.
+ * This is needed since Rest Routes just know and call GET PUT POST DELETE actions. 
+ * All other additionally actions must configured as either RestFakeRoute or RestLikeRoute
+ * 
+ * RestFakeRoute and RestLikeRoute Routes are treated in error and access handling like a a rest route (called via API)
+ * 
+ * Difference between RestFakeRoute and RestLikeRoute:
+ * RestFakeRoute: No automatic request result conversion is done - the result is outputted as generated (HTML, binary data from a file, JSON manually encoded)
+ * RestLikeRoute: REST_Controller_Plugin_RestHandler is invoked, so the data in the view is returned in the format as requested by the caller  (mainly JSON)
  */
 class ZfExtended_Controller_RestLikeRoute extends Zend_Controller_Router_Route {
 }
