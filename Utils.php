07260e5f847934f60ca49fced9900b8c9cb3301e
aleksandar <aleksandar@mittagqi.com>
2018-10-16 12:27:29 +0200

    TRANSLATE-1342 Administration of engines and resources, customers and users (rename tmmt controller and models)

--- Utils.php
+++ Utils.php
@@ -149,4 +149,45 @@ class ZfExtended_Utils {
         }
         return 'development';
     }
+    
+    /**
+     * splits the text up into HTML tags / entities on one side and plain text on the other side
+     * The order in the array is important for the following wordBreakUp, since there are HTML tags and entities ignored.
+     * Caution: The ignoring is done by the array index calculation there!
+     * So make no array structure changing things between word and tag break up!
+     *
+     * @param string $text
+     * @return array $text
+     */
+    public static  function tagBreakUp($tagRegex,$text){
+        if(is_null($tagRegex)){
+            throw new Zend_Exception('Regex zur Tagerkennung ist NULL');
+        }
+        return preg_split($tagRegex, $text, NULL,  PREG_SPLIT_DELIM_CAPTURE);
+    }
+    
+    /**
+     * Zerlegt die Wortteile des segment-Arrays anhand der Wortgrenzen in ein Array,
+     * welches auch die Worttrenner als jeweils eigene Arrayelemente enthält
+     *
+     * - parst nur die geraden Arrayelemente, denn dies sind die Wortbestandteile
+     *   (aber nur weil tagBreakUp davor aufgerufen wurde und sonst das array nicht in der Struktur verändert wurde)
+     *
+     * @param array $segment
+     * @return array $segment
+     */
+    public static function wordBreakUp($segment){
+        $config = Zend_Registry::get('config');
+        $regexWordBreak = $config->runtimeOptions->editor->export->wordBreakUpRegex;
+        
+        //by adding the count($split) and the $i++ only the array entries containing text (no tags) are parsed
+        //this implies that only tagBreakUp may be called before and
+        // no other array structure manipulating method may be called between tagBreakUp and wordBreakUp!!!
+        for ($i = 0; $i < count($segment); $i++) {
+            $split = preg_split($regexWordBreak, $segment[$i], NULL, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
+            array_splice($segment, $i, 1, $split);
+            $i = $i + count($split);
+        }
+        return $segment;
+    }
 }
