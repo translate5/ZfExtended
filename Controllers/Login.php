5a8f29b258733d892353c9a722786dacd6541350
Thomas Lauria <thomas@marcmittag.de>
2019-02-25 10:15:35 +0100

    code clean up

--- Controllers/Login.php
+++ Controllers/Login.php
@@ -324,7 +324,7 @@ abstract class ZfExtended_Controllers_Login extends ZfExtended_Controllers_Actio
             $this->view->form = $this->_form;
             return;
         }
-        $this->_redirect('/login');
+        $this->redirect('/login');
     }
     
     /**
