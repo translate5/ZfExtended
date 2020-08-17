37bbbd7bd324e1ddfc85165654ee0b6340367a9e
Thomas Lauria <thomas@mittagqi.com>
2020-08-17 15:02:50 +0200

    TRANSLATE-2177: Commandline Interface to maintain translate5 - publish command by creating a translate5.sh/.bat shortcut - adopt all Standalone2 Modules, so thats deprecated now

--- Models/Installer/Modules/Task.php
+++ Models/Installer/Modules/Task.php
@@ -9,8 +9,8 @@ START LICENSE AND COPYRIGHT
  Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
 
  This file may be used under the terms of the GNU LESSER GENERAL PUBLIC LICENSE version 3
- as published by the Free Software Foundation and appearing in the file lgpl3-license.txt 
- included in the packaging of this file.  Please review the following information 
+ as published by the Free Software Foundation and appearing in the file lgpl3-license.txt
+ included in the packaging of this file.  Please review the following information
  to ensure the GNU LESSER GENERAL PUBLIC LICENSE version 3.0 requirements will be met:
 https://www.gnu.org/licenses/lgpl-3.0.txt
 
@@ -25,6 +25,7 @@ END LICENSE AND COPYRIGHT
 /**
  * @package ZfExtended
  * @version 2.0
+ * @deprecated
  */
 class ZfExtended_Models_Installer_Modules_Help extends ZfExtended_Models_Installer_Modules_Abstract{
     public function __construct($options){
