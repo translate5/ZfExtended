fadd025df79e31b01820a010e51dadf61b691722
aleksandar <aleksandar@mittagqi.com>
2020-10-16 13:16:42 +0200

    TRANSLATE-2245 Switch analysis to batch mode, where language resources support it

--- Zendoverwrites/Http/Client.php
+++ Zendoverwrites/Http/Client.php
@@ -22,7 +22,38 @@ https://www.gnu.org/licenses/lgpl-3.0.txt
 END LICENSE AND COPYRIGHT
 */
 
-class  ZfExtended_HttpClient extends Zend_Http_Client {
+class  ZfExtended_Zendoverwrites_Http_Client extends Zend_Http_Client {
+
+    /***
+     * When set to true, in post request with encoding method ENC_URLENCODED,
+     * the array indexes will be removed from multivalue fields
+     * @var boolean
+     */
+    protected $removeArrayIndexInUrlEncode = false;
+    
+    
+    public function request($method = null){
+        
+        //ignore the debugginf if not enabled
+        if(!$this->isRequestDebugEnabled()){
+            return parent::request($method);
+        }
+        //TODO: add posibility this to be filtered per url
+        $randKey = substr(md5(rand()), 0, 7);
+        
+        error_log("Method ($randKey): ".(empty($method) ? $this->method : $method));
+        error_log("URL ($randKey):".$this->getUri(true));
+        error_log("\n\nDATA ($randKey): \n".$this->raw_post_data."\n\n");
+        error_log("Bytes ($randKey):".mb_strlen($this->raw_post_data));
+        error_log("Headers ".print_r($this->headers,1));
+        $response = parent::request($method);
+        error_log("Status ($randKey): ".print_r($response->getStatus(),1));
+        error_log("Raw Body ($randKey):".print_r($response->getRawBody(),1));
+        error_log("Body ($randKey):".print_r($response->getBody(),1));
+        error_log("Headers ($randKey):".($response->getHeadersAsString()));
+        return $response;
+    }
+    
     /**
      * Prepare the request body (for POST and PUT requests)
      *
@@ -106,7 +137,10 @@ class  ZfExtended_HttpClient extends Zend_Http_Client {
                         // Encode body as application/x-www-form-urlencoded
                         $this->setHeaders(self::CONTENT_TYPE, self::ENC_URLENCODED);
                         $body = http_build_query($this->paramsPost, '', '&');
-                        $body = preg_replace('/\%5B\d+\%5D/', '', $body);
+                        //remove the encoded array indexes from the body
+                        if($this->removeArrayIndexInUrlEncode){
+                            $body = preg_replace('/\%5B\d+\%5D/', '', $body);
+                        }
                         break;
                         
                     default:
@@ -134,4 +168,19 @@ class  ZfExtended_HttpClient extends Zend_Http_Client {
             return $body;
     }
     
+    /***
+     * @param bool $flag
+     */
+    public function setRemoveArrayIndexInUrlEncode(bool $flag){
+        $this->removeArrayIndexInUrlEncode = $flag;
+    }
+
+    /***
+     * Check if the debug mode is enabled for the current request.
+     * @return boolean
+     */
+    public function isRequestDebugEnabled(){
+        $config = Zend_Registry::get('config');
+        return $config->runtimeOptions->debug->httpclient ?? false;
+    }
 }
