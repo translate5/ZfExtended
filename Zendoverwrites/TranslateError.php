8f3fc373881dfc4739d6aeaa5745cf312551f969
Thomas Lauria <thomas@marcmittag.de>
2020-11-24 17:03:30 +0100

    Fixes for usage without editor module.

--- Zendoverwrites/TranslateError.php
+++ Zendoverwrites/TranslateError.php
@@ -9,8 +9,8 @@ START LICENSE AND COPYRIGHT
  Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
 
  This file may be used under the terms of the GNU LESSER GENERAL PUBLIC LICENSE version 3
- as published by the Free Software Foundation and appearing in the file lgpl3-license.txt 
- included in the packaging of this file.  Please review the following information 
+ as published by the Free Software Foundation and appearing in the file lgpl3-license.txt
+ included in the packaging of this file.  Please review the following information
  to ensure the GNU LESSER GENERAL PUBLIC LICENSE version 3.0 requirements will be met:
 https://www.gnu.org/licenses/lgpl-3.0.txt
 
@@ -34,7 +34,9 @@ END LICENSE AND COPYRIGHT
  */
 class  ZfExtended_Zendoverwrites_TranslateError extends ZfExtended_Zendoverwrites_Translate {
     public function __construct(Exception $e) {
-        //the logged error here show only why Translate could not be instanced, 
+        $this->config = Zend_Registry::get('config');
+        
+        //the logged error here show only why Translate could not be instanced,
         // it does not show the error which was leading to use the Translation Object in ErrorController
         error_log("Error on instancing ZfExtended_Zendoverwrites_Translate, error was: ".$e->getMessage().' in '.$e->getTraceAsString());
     }
