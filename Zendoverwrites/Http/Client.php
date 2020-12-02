bc6a3a4a95248c87fd4a8287c2712f08683c709e
Thomas Lauria <thomas@mittagqi.com>
2020-12-02 14:32:32 +0100

    TRANSLATE-2294: Handle additional tags - improve http client debugging

--- Zendoverwrites/Http/Client.php
+++ Zendoverwrites/Http/Client.php
@@ -9,8 +9,8 @@ START LICENSE AND COPYRIGHT
  Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
 
  This file may be used under the terms of the GNU LESSER GENERAL PUBLIC LICENSE version 3
- as published by the Free Software Foundation and appearing in the file lgpl3-license.txt 
- included in the packaging of this file.  Please review the following information 
+ as published by the Free Software Foundation and appearing in the file lgpl3-license.txt
+ included in the packaging of this file.  Please review the following information
  to ensure the GNU LESSER GENERAL PUBLIC LICENSE version 3.0 requirements will be met:
 https://www.gnu.org/licenses/lgpl-3.0.txt
 
@@ -38,24 +38,31 @@ class  ZfExtended_Zendoverwrites_Http_Client extends Zend_Http_Client {
     
     
     public function request($method = null){
-        
-        //ignore the debugginf if not enabled
-        if(!$this->isRequestDebugEnabled()){
+        //ignore the debugging if not enabled
+        $url = $this->getUri(true);
+        if(!$this->isRequestDebugEnabled($url)){
+        //if(!$this->isRequestDebugEnabled()){
             return parent::request($method);
         }
-        //TODO: add posibility this to be filtered per url
         $randKey = substr(md5(rand()), 0, 7);
         
-        error_log("Method ($randKey): ".(empty($method) ? $this->method : $method));
-        error_log("URL ($randKey):".$this->getUri(true));
-        error_log("\n\nDATA ($randKey): \n".$this->raw_post_data."\n\n");
-        error_log("Bytes ($randKey):".mb_strlen($this->raw_post_data));
+        if(!empty($this->paramsGet)) {
+            $url .= ('?'.http_build_query($this->paramsGet));
+        }
+        error_log("Request ($randKey): ".(empty($method) ? $this->method : $method).' '.$url);
         error_log("Headers ".print_r($this->headers,1));
+        if(!empty($this->raw_post_data)) {
+            $bytes = '('.mb_strlen($this->raw_post_data).' bytes)';
+            error_log("Raw Data ($randKey) '.$bytes.': \n".$this->raw_post_data."\n\n");
+        }
+        if(!empty($this->paramsPost)) {
+            error_log("Post Data ($randKey): \n".print_r($this->paramsPost,1));
+        }
         $response = parent::request($method);
         error_log("Status ($randKey): ".print_r($response->getStatus(),1));
+        error_log("Headers ($randKey):".($response->getHeadersAsString()));
         error_log("Raw Body ($randKey):".print_r($response->getRawBody(),1));
         error_log("Body ($randKey):".print_r($response->getBody(),1));
-        error_log("Headers ($randKey):".($response->getHeadersAsString()));
         return $response;
     }
     
@@ -68,19 +75,33 @@ class  ZfExtended_Zendoverwrites_Http_Client extends Zend_Http_Client {
     protected function _prepareBody()
     {
         $removeArrayIndexInUrlEncode = !empty($this->config['removeArrayIndexInUrlEncode']);
-                        //remove the encoded array indexes from the body
+        //remove the encoded array indexes from the body
         if($removeArrayIndexInUrlEncode && $this->enctype == self::ENC_URLENCODED){
             return preg_replace('/\%5B\d+\%5D/', '', parent::_prepareBody());
-                        }
+        }
         return parent::_prepareBody();
     }
-
+    
     /***
      * Check if the debug mode is enabled for the current request.
+     * @param $url string the URL to be called for filtering
      * @return boolean
      */
-    protected function isRequestDebugEnabled(){
+    protected function isRequestDebugEnabled(string $url): bool{
         $config = Zend_Registry::get('config');
-        return $config->runtimeOptions->debug->httpclient ?? false;
+        $debug = $config->debug->httpclient ?? false;
+        if(!$debug) {
+            return false;
+        }
+        $debug = strtolower($debug);
+        switch ($debug) {
+            case '1':
+            case 'on':
+            case 'true':
+                return true;
+                
+            default:
+                return (stripos($url, $debug) !== false);
+        }
     }
 }
