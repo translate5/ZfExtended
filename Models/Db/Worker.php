a2d31ee2b3659457fe6211542f8744d3cbba2b2a
Thomas Lauria <thomas@mittagqi.com>
2021-06-09 14:07:15 +0200

    TRANSLATE-2500: Worker Architecture: Solving Problems with Deadlocks and related Locking/Mutex Quirks

--- Models/Db/Worker.php
+++ Models/Db/Worker.php
@@ -9,8 +9,8 @@ START LICENSE AND COPYRIGHT
  Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
 
  This file may be used under the terms of the GNU LESSER GENERAL PUBLIC LICENSE version 3
- as published by the Free Software Foundation and appearing in the file lgpl3-license.txt 
- included in the packaging of this file.  Please review the following information 
+ as published by the Free Software Foundation and appearing in the file lgpl3-license.txt
+ included in the packaging of this file.  Please review the following information
  to ensure the GNU LESSER GENERAL PUBLIC LICENSE version 3.0 requirements will be met:
 https://www.gnu.org/licenses/lgpl-3.0.txt
 
@@ -34,4 +34,14 @@ END LICENSE AND COPYRIGHT
 class ZfExtended_Models_Db_Worker extends Zend_Db_Table_Abstract {
     protected $_name    = 'Zf_worker';
     public $_primary = 'id';
+    
+    /**
+     * By prepending this function call to update/delete queries, dead locks may be reduced there.
+     * According to https://dev.mysql.com/doc/refman/8.0/en/innodb-transaction-isolation-levels.html
+     * the usage of READ COMMITTED reduces the risk of dead locks for update and delete statements,
+     * since record locks are released after evaluating the WHERE condition of the statement with that level.
+     */
+    public function reduceDeadlocks() {
+        $this->getAdapter()->query('SET TRANSACTION ISOLATION LEVEL READ COMMITTED');
+    }
 }
