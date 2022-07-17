a93c217e2d580332a8d960afd5e38918451abc5b
Axel <axel@mittagqi.com>
2022-07-18 00:08:18 +0200

    minor

--- Models/Entity/Abstract.php
+++ Models/Entity/Abstract.php
@@ -159,7 +159,7 @@ abstract class ZfExtended_Models_Entity_Abstract {
         if (!$rowset || $rowset->count() == 0) {
             $this->notFound('#PK', $id);
         }
-        $rowset->rewind(); // triggers loading
+        $rowset->rewind();
         //load implies loading one Row, so use only the first row
         return $this->row = $rowset->current();
     }
