77288e58fc8e0f069def1611134a7314904e6fe9
Axel <axel@mittagqi.com>
2022-08-23 17:26:04 +0200

    TRANSLATE-3030-fixes-spellcheck-autoqa-worker * refactored update of progress: A model update was called twice in sequence what could be pulled together

--- Models/Worker.php
+++ Models/Worker.php
@@ -376,7 +376,7 @@ class ZfExtended_Models_Worker extends ZfExtended_Models_Entity_Abstract {
      * Set all Workers not yet finished as done for the current task & worker without the worker calling this API
      */
     public function setRemainingToDone() {
-        // set unfinished workers to done albeit the passed one
+        // set unfinished workers to done
         $bindings = [ self::STATE_DONE, $this->getTaskGuid(), $this->getWorker(), $this->getId(), self::STATE_PREPARE, self::STATE_SCHEDULED, self::STATE_WAITING, self::STATE_RUNNING ];
         $sql = 'UPDATE `Zf_worker` SET `state` = ?  WHERE `taskGuid` = ? AND `worker` = ? AND `id` != ? AND `state` IN (?, ?, ?, ?)';
         $this->retryOnDeadlock(function() use ($sql, $bindings){
