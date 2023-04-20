cb245842fcb94979ee504c029c20fa3841c30858
aleksandar <aleksandar@mittagqi.com>
2023-04-20 08:53:09 +0200

    TRANSLATE-3061 FIX API Tests - change rights for zip packages in api test

--- Test/ApiHelper.php
+++ Test/ApiHelper.php
@@ -662,7 +662,10 @@ class ZfExtended_Test_ApiHelper {
         $zip->open($pathToZip);
         $dir = sys_get_temp_dir().DIRECTORY_SEPARATOR.'translate5Test'.DIRECTORY_SEPARATOR;
         $this->rmDir($dir);
-        mkdir($dir);
+        if (!mkdir($dir) && !is_dir($dir)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $dir));
+        }
+        chmod($dir,0777);
         $zip->extractTo($dir);
         $files = glob($dir.$pathToFileInZip, GLOB_NOCHECK);
         $file = reset($files);
