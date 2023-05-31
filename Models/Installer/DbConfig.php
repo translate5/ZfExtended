f604f1559906adcaeee414df6492358435458a2b
Pavel Perminov <pavel.perminov.23@gmail.com>
2023-05-31 12:19:51 +0300

    https://jira.translate5.net/browse/TRANSLATE-3329 Testing certain branch in the cloud accessible for developers
    1.added ZfExtended_Models_Installer_DbConfig
    2.ZfExtended_Models_Installer_DbUpdater
     - refactored createDatabase()
       - *_DbConfig-instance is now used as 1s arg

---
+++ Models/Installer/DbConfig.php
@@ -0,0 +1,69 @@
+<?php
+class ZfExtended_Models_Installer_DbConfig {
+
+    /**
+     * Hostname
+     *
+     * @var string
+     */
+    public string $host;
+
+    /**
+     * Username
+     *
+     * @var string
+     */
+    public string $username;
+
+    /**
+     * Password
+     *
+     * @var string
+     */
+    public string $password;
+
+    /**
+     * Port number
+     *
+     * @var int
+     */
+    public int $port;
+
+    /**
+     * Database name
+     *
+     * @var string
+     */
+    public string $dbname;
+
+    /**
+     * Initialise from array
+     *
+     * @param array $params
+     * @return $this
+     */
+    public function initFromArray(array $params) {
+
+        // Setup params
+        foreach (['host', 'username', 'password', 'port', 'dbname'] as $param) {
+            if (isset($params[$param])) {
+                $this->$param = $params[$param];
+            }
+        }
+
+        // Return instance itself
+        return $this;
+    }
+
+    /**
+     * Prepare and return string to be used as $dsn argument for new PDO() call
+     *
+     * @see https://www.php.net/manual/en/pdo.construct.php
+     */
+    public function toPdoString() {
+        $pdo = 'mysql:host=' . $this->host;
+        if ($this->port) $pdo .= ';port=' . $this->port;
+        if ($this->dbname) $pdo .= ';dbname=' . $this->dbname;
+        return $pdo;
+    }
+}
