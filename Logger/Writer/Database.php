c5b0a6df654e736ad3d9a2b38f8f8627af683cf0
Thomas Lauria <thomas@marcmittag.de>
2023-07-05 21:09:18 +0200

    - fix is_a order - fix coding style and minor issues

--- Logger/Writer/Database.php
+++ Logger/Writer/Database.php
@@ -9,8 +9,8 @@ START LICENSE AND COPYRIGHT
  Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
 
  This file may be used under the terms of the GNU LESSER GENERAL PUBLIC LICENSE version 3
- as published by the Free Software Foundation and appearing in the file lgpl3-license.txt 
- included in the packaging of this file.  Please review the following information 
+ as published by the Free Software Foundation and appearing in the file lgpl3-license.txt
+ included in the packaging of this file.  Please review the following information
  to ensure the GNU LESSER GENERAL PUBLIC LICENSE version 3.0 requirements will be met:
 https://www.gnu.org/licenses/lgpl-3.0.txt
 
@@ -24,49 +24,66 @@ END LICENSE AND COPYRIGHT
 
 /**
  */
-class ZfExtended_Logger_Writer_Database extends ZfExtended_Logger_Writer_Abstract {
-    
-    protected $insertedId;
-    
-    public function write(ZfExtended_Logger_Event $event) {
+class ZfExtended_Logger_Writer_Database extends ZfExtended_Logger_Writer_Abstract
+{
+
+    protected string $insertedId;
+
+    public function write(ZfExtended_Logger_Event $event): void
+    {
         $db = ZfExtended_Factory::get('ZfExtended_Models_Db_ErrorLog');
         /* @var $db ZfExtended_Models_Db_ErrorLog */
-        
+
         //if this event is a duplication, we update the entry and do not insert a new one
         $duplicateCount = $this->getDuplicateCount($event);
-        if($duplicateCount > 0 && $db->incrementDuplicate($event->duplicationHash, $duplicateCount)) {
+        if ($duplicateCount > 0 && $db->incrementDuplicate($event->duplicationHash, $duplicateCount)) {
             return;
         }
-        
+
         //get this data directly from the event:
-        $directlyFromEvent = ['level', 'domain', 'worker', 'eventCode', 'message', 'appVersion', 'file', 'line', 'trace', 'httpHost', 'url', 'method', 'userLogin', 'userGuid'];
+        $directlyFromEvent = [
+            'level',
+            'domain',
+            'worker',
+            'eventCode',
+            'message',
+            'appVersion',
+            'file',
+            'line',
+            'trace',
+            'httpHost',
+            'url',
+            'method',
+            'userLogin',
+            'userGuid'
+        ];
+
         $data = [];
-        foreach($directlyFromEvent as $key) {
+        foreach ($directlyFromEvent as $key) {
             $data[$key] = $event->$key;
         }
-        if(!empty($event->exception)) {
-            $data['message'] = get_class($event->exception).': '.$data['message'];
+        if (!empty($event->exception)) {
+            $data['message'] = get_class($event->exception) . ': ' . $data['message'];
         }
-        
+
         $data['message'] = mb_substr($data['message'], 0, 512);
         $data['last'] = NOW_ISO;
         $data['duplicates'] = 0;
         $data['duplicateHash'] = $event->duplicationHash;
-        
+
         //we track previous exceptions seperate in the DB if possible
-        if(!empty($event->previous)) {
-            if($event->previous instanceof ZfExtended_Logger_Event) {
+        if (!empty($event->previous)) {
+            if ($event->previous instanceof ZfExtended_Logger_Event) {
                 $this->write($event->previous);
-                $event->extra['_previous_exception_id'] = '#'.$this->insertedId;
-            }
-            elseif($event->previous instanceof Exception){
+                $event->extra['_previous_exception_id'] = '#' . $this->insertedId;
+            } elseif ($event->previous instanceof Exception) {
                 $event = clone $event;
-                $event->extra['_previous_exception'] = (string) $event->previous;
+                $event->extra['_previous_exception'] = (string)$event->previous;
             }
         }
-        
+
         //flatten entities to their dataobjects and handles JSON errors:
         $data['extra'] = $event->getExtraAsJson();
-        $this->insertedId = $db->insert($data);
+        $this->insertedId = (string) $db->insert($data);
     }
 }
--- Logger/Writer/ErrorLog.php
+++ Logger/Writer/ErrorLog.php
@@ -9,8 +9,8 @@ START LICENSE AND COPYRIGHT
  Contact:  http://www.MittagQI.com/  /  service (ATT) MittagQI.com
 
  This file may be used under the terms of the GNU LESSER GENERAL PUBLIC LICENSE version 3
- as published by the Free Software Foundation and appearing in the file lgpl3-license.txt 
- included in the packaging of this file.  Please review the following information 
+ as published by the Free Software Foundation and appearing in the file lgpl3-license.txt
+ included in the packaging of this file.  Please review the following information
  to ensure the GNU LESSER GENERAL PUBLIC LICENSE version 3.0 requirements will be met:
 https://www.gnu.org/licenses/lgpl-3.0.txt
 
@@ -24,24 +24,25 @@ END LICENSE AND COPYRIGHT
 
 /**
  */
-class ZfExtended_Logger_Writer_ErrorLog extends ZfExtended_Logger_Writer_Abstract {
-    public function write(ZfExtended_Logger_Event $event) {
+class ZfExtended_Logger_Writer_ErrorLog extends ZfExtended_Logger_Writer_Abstract
+{
+    public function write(ZfExtended_Logger_Event $event): void
+    {
         //FIXME duplicate handling:print all? ignore all duplicates?
-          // print one line prefixed with repeated >10/>50/100/>200/>500/>1000 times
+        // print one line prefixed with repeated >10/>50/100/>200/>500/>1000 times
 //         $duplicateCount = $this->getDuplicateCount($event);
 //         if($duplicateCount > 0) {
 //             $db->incrementDuplicate($event->duplicationHash, $duplicateCount);
 //             return;
 //         }
-        if(in_array($event->eventCode,['E9999','E0001','E0002'])) {
-            if(is_dir(APPLICATION_ROOT.'/.idea')) {
+        if (in_array($event->eventCode, ['E9999', 'E0001', 'E0002'])) {
+            if (is_dir(APPLICATION_ROOT . '/.idea')) {
                 //change trace so that phpstorm can directly jump to
-                $event = preg_replace('/([\s]+#[0-9]+ )([^(]+)\(([0-9]+)\):/', "$1in file://$2:$3 ", $event);
-                $event = preg_replace('#([\s]+in [^/]+)(/[^(]+) \(([0-9]+)\)#', "$1in file://$2:$3 ", $event);
+                $event = preg_replace('/(\s+#\d+ )([^(]+)\((\d+)\):/', "$1in file://$2:$3 ", $event);
+                $event = preg_replace('#(\s+in [^/]+)(/[^(]+) \((\d+)\)#', "$1in file://$2:$3 ", $event);
             }
             error_log($event);
-        }
-        else {
+        } else {
             error_log($event->oneLine());
         }
     }
