992af399d81d45a0d0fb6f13ab412715c5a55a8c
aleksandar <aleksandar@mittagqi.com>
2023-09-29 15:35:27 +0200

    TRANSLATE-3477 Add missing ACL right and role documentation  - fix backend acl reference

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -22,7 +22,7 @@ https://www.gnu.org/licenses/lgpl-3.0.txt
 END LICENSE AND COPYRIGHT
 */
 
-use MittagQI\ZfExtended\Acl\Roles;
+use MittagQI\Translate5\Acl\Rights;
 use MittagQI\ZfExtended\Acl\SetAclRoleResource;
 use MittagQI\ZfExtended\Acl\SystemResource;
 
@@ -504,7 +504,7 @@ class ZfExtended_UserController extends ZfExtended_RestController {
     protected function checkUserAccessByParent(): void
     {
         //Am I allowed to see any user:
-        if($this->isAllowed(SystemResource::ID, SystemResource::SEE_ALL_USERS)) {
+        if($this->isAllowed(Rights::ID, SystemResource::SEE_ALL_USERS)) {
             return;
         }
 
