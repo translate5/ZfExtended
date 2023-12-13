42eebf0b98371267ba5a3a061d6722f64ea91799
Aleksandar Mitrev <aleksandar@mittagqi.com>
2023-12-13 14:55:11 +0000

    Merge pull request #231 in mittagqi/zfextended from bugfix/TRANSLATE-3597-fix-php-8.1-warnings to master
    TRANSLATE-3597 fix php-8.1 warnings
    
    * - code formating
    * TRANSLATE-3597 fix php-8.1 warnings
    
    Approved by Thomas Lauria

--- Languages.php
+++ Languages.php
@@ -41,7 +41,8 @@ END LICENSE AND COPYRIGHT
  * @method string getIso6393()
  * @method void setIso6393(string $lang)
  */
-abstract class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract {
+abstract class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract
+{
 
     /**
      * Retrieves the primary language of a RFC 5646 language (e.g. "en" from "en-GB")
@@ -62,8 +63,9 @@ abstract class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract {
      * @var Zend_Cache_Core
      */
     protected $memCache;
-    
-    public function __construct() {
+
+    public function __construct()
+    {
         parent::__construct();
         $this->memCache = Zend_Cache::factory('Core', new ZfExtended_Cache_MySQLMemoryBackend(), ['automatic_serialization' => true]);
     }
@@ -82,7 +84,8 @@ abstract class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract {
      * @param string $lang
      * @return Zend_Db_Table_Row_Abstract | null
      */
-    public function loadByRfc5646($lang){
+    public function loadByRfc5646($lang)
+    {
         return $this->loader($lang, self::LANG_TYPE_RFC5646);
     }
 
@@ -91,7 +94,8 @@ abstract class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract {
      * @param string $lcid
      * @return Zend_Db_Table_Row_Abstract | null
      */
-    public function loadByLcid($lcid){
+    public function loadByLcid($lcid)
+    {
         return $this->loader($lcid, self::LANG_TYPE_LCID);
     }
 
@@ -100,51 +104,55 @@ abstract class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract {
      * @param int $id
      * @return Zend_Db_Table_Row_Abstract | null
      */
-    public function loadById($id){
+    public function loadById($id)
+    {
         return $this->loader($id, self::LANG_TYPE_ID);
     }
 
     /**
      * loads the languages by the given DB ID's
-     * @param mixed $id's
+     * @param mixed $id 's
      * @return array | null
      */
-    public function loadByIds($ids){
+    public function loadByIds($ids)
+    {
         return $this->loaderByIds($ids, self::LANG_TYPE_ID);
     }
-    
-    /***
+
+    /**
      * Load languages by rfc values
      * @param array $rfc
      * @return array
      */
-    public function loadByRfc(array $rfc){
+    public function loadByRfc(array $rfc)
+    {
         $s = $this->db->select();
-        $s->where('lower(rfc5646) IN(?)',$rfc);
+        $s->where('lower(rfc5646) IN(?)', $rfc);
         $retval = $this->db->fetchAll($s)->toArray();
-        if(empty($retval)){
+        if (empty($retval)) {
             return [];
         }
         return $retval;
     }
+
     /**
      * @param mixed $lang
      * @param string $field
      * @return Zend_Db_Table_Row_Abstract | null
      */
-    protected function loader($lang, $field) {
+    protected function loader($lang, $field)
+    {
         $s = $this->db->select();
         $field = strtolower($field);
-        if($field == self::LANG_TYPE_ID || $field == self::LANG_TYPE_LCID) {
-            $s->where($field.' = ?', $lang);
+        if ($field == self::LANG_TYPE_ID || $field == self::LANG_TYPE_LCID) {
+            $s->where($field . ' = ?', $lang);
+        } else {
+            $s->where('lower(' . $field . ') = ?', strtolower($lang));
         }
-        else {
-            $s->where('lower('.$field.') = ?',strtolower($lang));
-        }
-        
+
         $this->row = $this->db->fetchRow($s);
-        if(empty($this->row)){
-            $this->notFound('#by'.ucfirst($field), $lang);
+        if (empty($this->row)) {
+            $this->notFound('#by' . ucfirst($field), $lang);
         }
         return $this->row;
     }
@@ -154,23 +162,23 @@ abstract class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract {
      * @param string $field
      * @return array | null
      */
-    protected function loaderByIds($langs, $field) {
+    protected function loaderByIds($langs, $field)
+    {
         $s = $this->db->select();
         //FIXME convert that to array only after merge of TRANSLATE-2834
-        if(is_array($langs)) {
-            $s->where($field.' IN(?)', $langs);
-        }
-        else{
-        $s->where($field.' IN('.$langs.')');
+        if (is_array($langs)) {
+            $s->where($field . ' IN(?)', $langs);
+        } else {
+            $s->where($field . ' IN(' . $langs . ')');
         }
         $s->order('rfc5646 ASC');
         $retval = $this->db->fetchAll($s)->toArray();
-        if(empty($retval)){
-            $this->notFound('#by'.ucfirst($field), $langs);
+        if (empty($retval)) {
+            $this->notFound('#by' . ucfirst($field), $langs);
         }
         return $retval;
     }
-    
+
     /**
      * Returns all configured languages in an array for displaying in frontend
      * in format :
@@ -182,29 +190,31 @@ abstract class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract {
      *                  [text] => Mazedonisch (mk)
      *                )
      */
-    public function getAvailableLanguages() {
+    public function getAvailableLanguages()
+    {
         $langs = $this->loadAll();
         $result = array();
         foreach ($langs as $lang) {
-            $name =$lang['langName'];
+            $name = $lang['langName'];
             $result[$name] = array(
-                    'id'=>$lang['id'],
-                    'value'=>$lang['rfc5646'],
-                    'text'=>$name.' ('.$lang['rfc5646'].')');
+                'id' => $lang['id'],
+                'value' => $lang['rfc5646'],
+                'text' => $name . ' (' . $lang['rfc5646'] . ')');
         }
         ksort($result); //sort by name of language
-        if(empty($result)){
+        if (empty($result)) {
             throw new Zend_Exception('No languages defined. Please use /docs/003fill-LEK-languages-after-editor-sql or define them otherwhise.');
         }
         return array_values($result);
     }
-    
+
     /**
      * Gibt die interne Sprach ID anhand der übergebenen Sprache im spezifizierten Typ zurück
      * @param mixed $lang
      * @param string $type
      */
-    public function getLangId($lang, $type = self::LANG_TYPE_RFC5646) {
+    public function getLangId($lang, $type = self::LANG_TYPE_RFC5646)
+    {
         $this->loadLang($lang, $type);
         return $this->getId();
     }
@@ -214,7 +224,8 @@ abstract class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract {
      * @param int $lcid LCID, wie in Tabelle languages hinterlegt
      * @return int id der gesuchten Sprache
      */
-    public function getLangIdByLcid($lcid){
+    public function getLangIdByLcid($lcid)
+    {
         $this->loadByLcid($lcid);
         return $this->getId();
     }
@@ -224,38 +235,42 @@ abstract class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract {
      * @param int $lang Sprachkürzel nach RFC5646, wie in Tabelle languages hinterlegt
      * @return int id der gesuchten Sprache
      */
-    public function getLangIdByRfc5646($lang){
+    public function getLangIdByRfc5646($lang)
+    {
         $this->loadByRfc5646($lang);
         return $this->getId();
     }
-    
+
     /**
      * Gibt die interne default Sublanguage zu einem Sprachkürzel nach RFC5646 zurück
      * @param int $lang Sprachkürzel nach RFC5646, wie in Tabelle languages hinterlegt
      * @return string sublanguage der gesuchten Sprache
      */
-    public function getSublanguageByRfc5646($lang){
+    public function getSublanguageByRfc5646($lang)
+    {
         $this->loadByRfc5646($lang);
         return $this->getSublanguage();
     }
-    
+
     /**
+     * @param string $lang Sprachkürzel nach RFC5646, wie in Tabelle languages hinterlegt
+     * @return string mainlanguage der gesuchten Sprache
      * @deprecated: Use static ::primaryCodeByRfc5646 API
      * Gibt den Sprachteil von RFC5646-Sprachkürzel zurück (unabh. vom Land),
      * z.B. für "de" für "de-AT" oder "de" für "de" oder "sr" für "Sr-Cyrl".
-     * @param string $lang Sprachkürzel nach RFC5646, wie in Tabelle languages hinterlegt
-     * @return string mainlanguage der gesuchten Sprache
      */
-    public function getMainlanguageByRfc5646($lang){
+    public function getMainlanguageByRfc5646($lang)
+    {
         return static::primaryCodeByRfc5646($lang);
     }
-    
+
     /**
      * Gibt die interne ISO_3166-1_alpha-2 zu einem Sprachkürzel nach RFC5646 zurück
      * @param int $lang Sprachkürzel nach RFC5646, wie in Tabelle languages hinterlegt
      * @return string ISO_3166-1_alpha-2 der gesuchten Sprache
      */
-    public function getIso3166Part1alpha2ByRfc5646($lang){
+    public function getIso3166Part1alpha2ByRfc5646($lang)
+    {
         $this->loadByRfc5646($lang);
         return $this->getIso3166Part1alpha2();
     }
@@ -266,7 +281,8 @@ abstract class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract {
      * @param string $type
      * @return Zend_Db_Table_Row_Abstract | null
      */
-    public function loadLang($lang, $type = self::LANG_TYPE_RFC5646) {
+    public function loadLang($lang, $type = self::LANG_TYPE_RFC5646)
+    {
         switch ($type) {
             case self::LANG_TYPE_ID:
                 return $this->loadById($lang);
@@ -284,8 +300,9 @@ abstract class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract {
      * @param string $lang
      * @return string
      */
-    public function getAutoDetectedType($lang) {
-        if(is_int($lang)) {
+    public function getAutoDetectedType($lang)
+    {
+        if (is_int($lang)) {
             return self::LANG_TYPE_LCID;
         }
         return self::LANG_TYPE_RFC5646;
@@ -297,33 +314,35 @@ abstract class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract {
      * @param array $preorderd
      * @return array
      */
-    public function orderLanguages($languages, array $preorderd){
-        if(empty($preorderd)){
+    public function orderLanguages($languages, array $preorderd)
+    {
+        if (empty($preorderd)) {
             return $languages;
         }
-        foreach ($preorderd as $lng){
+        foreach ($preorderd as $lng) {
             $oldIndex = array_search($lng, array_column($languages, 'value'));
-            if(!$oldIndex){
+            if (!$oldIndex) {
                 continue;
             }
-            $newIndex =array_search($lng, $preorderd);
+            $newIndex = array_search($lng, $preorderd);
             $tmp = $languages[$newIndex];
-            $languages[$newIndex] =$languages[$oldIndex];
+            $languages[$newIndex] = $languages[$oldIndex];
             $languages[$oldIndex] = $tmp;
         }
         return $languages;
     }
-    
+
     /**
      * Return language rfc5646 value for given language id
      * @param int $langId
      * @return string
      */
-    public function loadLangRfc5646($langId){
+    public function loadLangRfc5646($langId)
+    {
         $this->loadById($langId);
         return $this->getRfc5646();
     }
-    
+
     /**
      * Find languages which are belonging to the same language group
      * Ex:
@@ -333,14 +352,14 @@ abstract class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract {
      */
     public function findLanguageGroup($rfc): array
     {
-        $rfc = explode('-',$rfc);
+        $rfc = explode('-', $rfc);
         $rfc = strtolower($rfc[0]);
         $s = $this->db->select();
-        $s->where('lower(rfc5646) = ?',$rfc);
-        $s->orWhere('lower(rfc5646) LIKE ?',$rfc.'-%');
+        $s->where('lower(rfc5646) = ?', $rfc);
+        $s->orWhere('lower(rfc5646) LIKE ?', $rfc . '-%');
         $s->order('length(rfc5646)');
         $retval = $this->db->fetchAll($s)->toArray();
-        if(empty($retval)){
+        if (empty($retval)) {
             return [];
         }
         return $retval;
@@ -366,24 +385,23 @@ abstract class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract {
      */
     public function getFuzzyLanguages($id, string $field = 'id', bool $includeMajor = false): array
     {
-        $cacheId = __FUNCTION__.'_'.$id.'_'.$field.'_'.var_export($includeMajor,true).'_includeMajor';
+        $cacheId = __FUNCTION__ . '_' . $id . '_' . $field . '_' . var_export($includeMajor, true) . '_includeMajor';
         $result = $this->memCache->load($cacheId);
-        if($result !== false) {
+        if ($result !== false) {
             return $result;
         }
 
         $rfc = $this->loadLangRfc5646($id); //loads the language internally
         //if it is a sub language (de-DE), take only that but include the general (major) language (de) if set via param
-        if(str_contains($rfc, '-')){
+        if (str_contains($rfc, '-')) {
             $result = [$this->get($field)];
-            if($includeMajor){
+            if ($includeMajor) {
                 $major = $this->findMajorLanguage($rfc);
-                if(!empty($major)){
+                if (!empty($major)) {
                     $result[] = $major[$field];
                 }
             }
-        }
-        // if it is general language, include all sub languages too
+        } // if it is general language, include all sub languages too
         else {
             $result = array_column($this->findLanguageGroup($rfc), $field);
         }
@@ -391,7 +409,7 @@ abstract class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract {
         return $result;
     }
 
-    /***
+    /**
      * Search languages by given search string.
      * The search will provide any match on rfc5646 field.
      *
@@ -399,41 +417,43 @@ abstract class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract {
      * @param array $fields
      * @return array
      */
-    public function search(string $searchString,array $fields = []): array
+    public function search(string $searchString, array $fields = []): array
     {
         $s = $this->db->select();
-        if(!empty($fields)){
-            $s->from($this->tableName,$fields);
+        if (!empty($fields)) {
+            $s->from($this->tableName, $fields);
         }
-        $s->where('lower(rfc5646) LIKE lower(?)','%'.$searchString.'%');
+        $s->where('lower(rfc5646) LIKE lower(?)', '%' . $searchString . '%');
         return $this->db->fetchAll($s)->toArray();
     }
-    
-    /***
-     * Load all languages where the return array will be with $key(lek_languages field) as key and $value(lek_languages field) as value
+
+    /**
+     * Load all languages where the return array will be with $key(lek_languages field) as key
+     * and $value(lek_languages field) as value
      *
      * @param string $key
      * @param string $value
-     * @param bool $lowercase: lowercase key and value
+     * @param bool $lowercase : lowercase key and value
      * @return array
      */
-    public function loadAllKeyValueCustom(string $key,string $value,bool $lowercase=false){
-        $rfcToIsoLanguage=array();
-        if(!isset($key) || !isset($value)){
-            return $rfcToIsoLanguage;
+    public function loadAllKeyValueCustom(string $key, string $value, bool $lowercase = false): array
+    {
+        $keyValuePairs = [];
+        if (!isset($key) || !isset($value)) {
+            return $keyValuePairs;
         }
-        $lngs=$this->loadAll();
-        foreach($lngs as $l){
-            if($lowercase){
-                $rfcToIsoLanguage[strtolower($l[$key])] = empty($l[$value]) ? $l[$value] : strtolower($l[$value]);
-            }else{
-                $rfcToIsoLanguage[$l[$key]] = $l[$value];
+        $languages = $this->loadAll();
+        foreach ($languages as $l) {
+            if ($lowercase) {
+                $keyValuePairs[strtolower($l[$key] ?? '')] = strtolower($l[$value] ?? '');
+            } else {
+                $keyValuePairs[$l[$key]] = $l[$value];
             }
         }
-        return $rfcToIsoLanguage;
+        return $keyValuePairs;
     }
 
-    /***
+    /**
      * Load all languages for front-end display (language name + (rfc value) ).
      * Language array result layout:
      *  [
@@ -451,30 +471,32 @@ abstract class ZfExtended_Languages extends ZfExtended_Models_Entity_Abstract {
      * @return array
      * @throws Zend_Exception
      */
-    public function loadAllForDisplay(string $fieldKey = '', bool $onlyName = FALSE) {
-        $translate= ZfExtended_Zendoverwrites_Translate::getInstance();
+    public function loadAllForDisplay(string $fieldKey = '', bool $onlyName = FALSE)
+    {
+        $translate = ZfExtended_Zendoverwrites_Translate::getInstance();
         $langs = $this->loadAll();
         $result = [];
         foreach ($langs as $lang) {
             $name = $translate->_($lang['langName']);
             $key = empty($fieldKey) ? $name : $lang[$fieldKey];
-            $tempName = $name.(($onlyName !== TRUE) ? ' ('.$lang['rfc5646'].')' : '');
-            $result[$key] = [$lang['id'], $tempName, $lang['rtl'],$lang['rfc5646'], $lang['iso3166Part1alpha2']];
+            $tempName = $name . (($onlyName !== TRUE) ? ' (' . $lang['rfc5646'] . ')' : '');
+            $result[$key] = [$lang['id'], $tempName, $lang['rtl'], $lang['rfc5646'], $lang['iso3166Part1alpha2']];
         }
         ksort($result); //sort by name of language
-        if(empty($result)){
+        if (empty($result)) {
             throw new Zend_Exception('No languages defined. Please use /docs/003fill-LEK-languages-after-editor-sql or define them otherwhise.');
         }
         return empty($fieldKey) ? array_values($result) : $result;
     }
-    
-    /***
+
+    /**
      * Find mayor language by given sub langauge.
      * Ex: "de-CH" will find "de"
      * @param string $rfcSub
      * @return array
      */
-    public function findMajorLanguage(string $rfcSub) {
+    public function findMajorLanguage(string $rfcSub)
+    {
         $rfcSub = explode('-', $rfcSub);
         $rfcSub = $rfcSub[0];
         $mayor = $this->loadByRfc([$rfcSub]);
