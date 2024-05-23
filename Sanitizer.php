10da40258619d1b300ff70cbcef5536f3c949de3
Oleksandr Mikhliaiev <oleksandr@mittagqi.com>
2024-05-23 08:11:02 +0000

    Merge pull request #243 in mittagqi/zfextended from bugfix/T5DEV-300-Test-XSS-in-all-input-fields-of-the-application to develop
    T5DEV-300 Test XSS in all input fields of the application
    
    * Merged develop into bugfix/T5DEV-300-Test-XSS-in-all-input-fields-of-the-application
    * Merge branch 'bugfix/T5DEV-300-Test-XSS-in-all-input-fields-of-the-application' of bitbucket.org:mittagqi/ZfExtended into bugfix/T5DEV-300-Test-XSS-in-all-input-fields-of-the-application
    * Merge branch 'develop' of bitbucket.org:mittagqi/ZfExtended into bugfix/T5DEV-300-Test-XSS-in-all-input-fields-of-the-application
    * Merged develop into bugfix/T5DEV-300-Test-XSS-in-all-input-fields-of-the-application
    * Merged develop into bugfix/T5DEV-300-Test-XSS-in-all-input-fields-of-the-application
    * Merge branch master into 'bugfix/T5DEV-300-Test-XSS-in-all-input-fields-of-the-application'
    * Merge branch 'develop' of bitbucket.org:mittagqi/ZfExtended into bugfix/T5DEV-300-Test-XSS-in-all-input-fields-of-the-application
    * Merge branch 'bugfix/T5DEV-300-Test-XSS-in-all-input-fields-of-the-application' of bitbucket.org:mittagqi/ZfExtended into bugfix/T5DEV-300-Test-XSS-in-all-input-fields-of-the-application
    * T5DEV-300 Use Markup class for validation
    * T5DEV-300 Move Markup
    * Merge branch 'master' of bitbucket.org:mittagqi/ZfExtended into bugfix/T5DEV-300-Test-XSS-in-all-input-fields-of-the-application
    * Merged develop into bugfix/T5DEV-300-Test-XSS-in-all-input-fields-of-the-application
    * T5DEV-300 Throw exception on malformed markup provided
    * T5DEV-300 Add some new methods to Sanitizer.php
    
    Approved by Axel Becher
    Approved by Thomas Lauria

--- Sanitizer.php
+++ Sanitizer.php
@@ -22,6 +22,9 @@ https://www.gnu.org/licenses/lgpl-3.0.txt
 END LICENSE AND COPYRIGHT
 */
 
+use MittagQI\ZfExtended\Tools\Markup;
+use ZfExtended_SecurityException as SecurityException;
+
 /**
  * general sanitizer that sanitizes ALL request params
  */
@@ -46,7 +49,8 @@ final class ZfExtended_Sanitizer
     /**
      * Sanitizes a request value that represents the given type
      * The type must be one of our constants
-     * @throws ZfExtended_SecurityException
+     *
+     * @throws SecurityException
      */
     public static function sanitize(?string $val, string $type): ?string
     {
@@ -71,18 +75,26 @@ final class ZfExtended_Sanitizer
      * If unwanted contents are detected, an exception is thrown, otherwise the markup is returned
      * Note, that this function expects markup and not whole documents
      * Note, that invalid markup will be stripped and the text returned
-     * @throws ZfExtended_SecurityException
+     *
+     * @throws SecurityException
      */
     public static function markup(string $markup): string
     {
         $dom = new ZfExtended_Dom();
         $nodeList = $dom->loadUnicodeMarkup($markup);
+
+        if (!Markup::isValid($markup)) {
+            throw new SecurityException('Provided markup is malformed');
+        }
+
         // this is debatable: when invalid markup is posted, we remove it and use the posted text contents
-        // this is neccessary, as browsers are much more tolerant than DOMDocument, and we can not expect the broken markup to be "broken enough" to not make attacks possible
+        // this is neccessary, as browsers are much more tolerant than DOMDocument,
+        // and we can not expect the broken markup to be "broken enough" to not make attacks possible
         // may it is better to throw an "invalid markup exception" here ?
         if ($nodeList === null) {
             return strip_tags($markup);
         }
+
         foreach ($nodeList as $node) {
             self::checkNode($node);
         }
@@ -92,37 +104,122 @@ final class ZfExtended_Sanitizer
 
     /**
      * Checks a DOM node and throws an exception if something illegal was detected
-     * @throws ZfExtended_SecurityException
+     *
+     * @throws SecurityException
      */
-    private static function checkNode(DOMNode $node)
+    private static function checkNode(DOMNode $node): void
     {
-        if ($node->nodeType == XML_ELEMENT_NODE) {
-            if (strtolower($node->nodeName) === 'script') {
-                throw new ZfExtended_SecurityException('Script tags are not allowed in the sent markup');
-            }
-            if ($node->hasAttributes()) {
-                foreach ($node->attributes as $attribute) { /* @var $attribute DOMNode */
-                    $name = strtolower($attribute->nodeName);
-                    // any event-handler attribute will be rejected. We do not check, if this is actually a valid event-handler, so "onanything" will also be invalid
-                    if (strlen($name) > 2 && str_starts_with($name, 'on')) {
-                        throw new ZfExtended_SecurityException('Event-handler attributes are not allowed in the sent markup');
-                    }
-                    // old-school but still possible: attack with "javascript:" pseudo-protocol
-                    if ($name === 'href') {
-                        $href = preg_replace('/\s+/', '', strtolower($attribute->nodeValue));
-                        if (str_starts_with($href, 'javascript:')) {
-                            throw new ZfExtended_SecurityException('JavaScript-hrefs are not allowed in the sent markup');
-                        }
-                    }
-                }
-            }
-            if ($node->hasChildNodes()) {
-                foreach ($node->childNodes as $childNode) { /* @var $childNode DOMNode */
-                    self::checkNode($childNode);
-                }
-            }
-        } elseif ($node->nodeType == XML_DOCUMENT_NODE) {
-            throw new ZfExtended_SecurityException('Embedded documents are not allowed in the sent markup');
+        if ($node->nodeType == XML_DOCUMENT_NODE) {
+            throw new SecurityException('Embedded documents are not allowed in the sent markup');
         }
+
+        if ($node->nodeType != XML_ELEMENT_NODE) {
+            return;
+        }
+
+        if (strtolower($node->nodeName) === 'script') {
+            throw new SecurityException('Script tags are not allowed in the sent markup');
+        }
+
+        if ($node->hasChildNodes()) {
+            foreach ($node->childNodes as $childNode) {
+                /* @var $childNode DOMNode */
+                self::checkNode($childNode);
+            }
+        }
+
+        if (!$node->hasAttributes()) {
+            return;
+        }
+
+        /* @var $attribute DOMNode */
+        foreach ($node->attributes as $attribute) {
+            $name = strtolower($attribute->nodeName);
+
+            // any event-handler attribute will be rejected.
+            // We do not check, if this is actually a valid event-handler, so "onanything" will also be invalid
+            if (strlen($name) > 2 && str_starts_with($name, 'on')) {
+                throw new SecurityException('Event-handler attributes are not allowed in the sent markup');
+            }
+
+            if ($name !== 'href') {
+                continue;
+            }
+
+            // attack with "javascript:" pseudo-protocol
+            $href = preg_replace('/\s+/', '', strtolower($attribute->nodeValue));
+
+            if (str_starts_with($href, 'javascript:')) {
+                throw new SecurityException('JavaScript-hrefs are not allowed in the sent markup');
+            }
+        }
+    }
+
+    public static function escapeHtml(?string $content, ?string $exceptionRegex = null): ?string
+    {
+        if (empty($content)) {
+            return $content;
+        }
+
+        if (null === $exceptionRegex) {
+            return htmlspecialchars($content);
+        }
+
+        $hasInternalTags = false !== preg_match_all($exceptionRegex, $content, $tags, PREG_SET_ORDER);
+
+        if (!$hasInternalTags) {
+            return htmlspecialchars($content);
+        }
+
+        $textParts = preg_split($exceptionRegex, $content);
+
+        $result = '';
+        foreach ($textParts as $i => $part) {
+            $result .= htmlspecialchars($part, ENT_COMPAT, null, false);
+
+            if (isset($tags[$i])) {
+                $result .= $tags[$i][0];
+            }
+        }
+
+        return $result;
+    }
+
+    public static function escapeHtmlRecursive(?array $content, ?string $exceptionRegex = null): ?array
+    {
+        if (empty($content)) {
+            return $content;
+        }
+
+        $result = [];
+        foreach ($content as $key => $value) {
+            $result[$key] = match (true) {
+                is_array($value) => self::escapeHtmlRecursive($value, $exceptionRegex),
+                is_string($value) => self::escapeHtml($value, $exceptionRegex),
+                is_object($value) => self::escapeHtmlInObject($value, $exceptionRegex),
+                default => $value
+            };
+        }
+
+        return $result;
+    }
+
+    public static function escapeHtmlInObject(?object $content, ?string $exceptionRegex = null): ?object
+    {
+        if (empty($content)) {
+            return $content;
+        }
+
+        $result = new \stdClass();
+        foreach ((array)$content as $key => $value) {
+            $result->{$key} = match (true) {
+                is_array($value) => self::escapeHtmlRecursive($value, $exceptionRegex),
+                is_string($value) => self::escapeHtml($value, $exceptionRegex),
+                is_object($value) => self::escapeHtmlInObject($value, $exceptionRegex),
+                default => $value
+            };
+        }
+
+        return $result;
     }
 }
